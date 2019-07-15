a3c3ab278448a763e4c4bba83fde18952978a807
markus7017 <markus7017@gmail.com>
2019-07-15 22:02:48 +0200

    first additions for RGBW2, xml def for Smoke fixed

--- src/main/java/org/openhab/binding/shelly/internal/ShellyBindingConstants.java
+++ src/main/java/org/openhab/binding/shelly/internal/ShellyBindingConstants.java
@@ -23,91 +23,93 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 @NonNullByDefault
 public class ShellyBindingConstants {
 
-    private static final String           BINDING_ID                  = "shelly";
+    private static final String           BINDING_ID                   = "shelly";
 
     // List of all Thing Type UIDs
-    public static final ThingTypeUID      THING_TYPE_SHELLY1          = new ThingTypeUID(BINDING_ID, "shelly1");
-    public static final ThingTypeUID      THING_TYPE_SHELLY1PM        = new ThingTypeUID(BINDING_ID, "shsw1pm");
+    public static final ThingTypeUID      THING_TYPE_SHELLY1           = new ThingTypeUID(BINDING_ID, "shelly1");
+    public static final ThingTypeUID      THING_TYPE_SHELLY1PM         = new ThingTypeUID(BINDING_ID, "shsw1pm");
     // public static final ThingTypeUID THING_TYPE_SHELLY2 = new ThingTypeUID(BINDING_ID, "shellyswitch");
     // public static final ThingTypeUID THING_TYPE_SHELLY25 = new ThingTypeUID(BINDING_ID, "shellyswitch25");
-    public static final ThingTypeUID      THING_TYPE_SHELLY2_RELAY    = new ThingTypeUID(BINDING_ID, "shelly2-relay");
-    public static final ThingTypeUID      THING_TYPE_SHELLY2_ROLLER   = new ThingTypeUID(BINDING_ID, "shelly2-roller");
-    public static final ThingTypeUID      THING_TYPE_SHELLY4PRO       = new ThingTypeUID(BINDING_ID, "shelly4pro");
-    public static final ThingTypeUID      THING_TYPE_SHELLYPLUG       = new ThingTypeUID(BINDING_ID, "shellyplug");
-    public static final ThingTypeUID      THING_TYPE_SHELLYPLUGS      = new ThingTypeUID(BINDING_ID, "shellyplug-s");
-    public static final ThingTypeUID      THING_TYPE_SHELLYBULB_COLOR = new ThingTypeUID(BINDING_ID, "shellybulb-color");
-    public static final ThingTypeUID      THING_TYPE_SHELLYBULB_WHITE = new ThingTypeUID(BINDING_ID, "shellybulb-white");
-    public static final ThingTypeUID      THING_TYPE_SHELLYHT         = new ThingTypeUID(BINDING_ID, "shellyht");
-    public static final ThingTypeUID      THING_TYPE_SHELLYSENSE      = new ThingTypeUID(BINDING_ID, "shellysense");
-    public static final ThingTypeUID      THING_TYPE_SHELLYSMOKE      = new ThingTypeUID(BINDING_ID, "shellysmoke");
-    public static final ThingTypeUID      THING_TYPE_SHELLYRGW2       = new ThingTypeUID(BINDING_ID, "shellyrgbw2");
+    public static final ThingTypeUID      THING_TYPE_SHELLY2_RELAY     = new ThingTypeUID(BINDING_ID, "shelly2-relay");
+    public static final ThingTypeUID      THING_TYPE_SHELLY2_ROLLER    = new ThingTypeUID(BINDING_ID, "shelly2-roller");
+    public static final ThingTypeUID      THING_TYPE_SHELLY4PRO        = new ThingTypeUID(BINDING_ID, "shelly4pro");
+    public static final ThingTypeUID      THING_TYPE_SHELLYPLUG        = new ThingTypeUID(BINDING_ID, "shellyplug");
+    public static final ThingTypeUID      THING_TYPE_SHELLYPLUGS       = new ThingTypeUID(BINDING_ID, "shellyplug-s");
+    public static final ThingTypeUID      THING_TYPE_SHELLYBULB_COLOR  = new ThingTypeUID(BINDING_ID, "shellybulb-color");
+    public static final ThingTypeUID      THING_TYPE_SHELLYBULB_WHITE  = new ThingTypeUID(BINDING_ID, "shellybulb-white");
+    public static final ThingTypeUID      THING_TYPE_SHELLYHT          = new ThingTypeUID(BINDING_ID, "shellyht");
+    public static final ThingTypeUID      THING_TYPE_SHELLYSENSE       = new ThingTypeUID(BINDING_ID, "shellysense");
+    public static final ThingTypeUID      THING_TYPE_SHELLYSMOKE       = new ThingTypeUID(BINDING_ID, "shellysmoke");
+    public static final ThingTypeUID      THING_TYPE_SHELLYRGBW2_COLOR = new ThingTypeUID(BINDING_ID, "shellyrgbw2-color");
+    public static final ThingTypeUID      THING_TYPE_SHELLYRGBW2_WHITE = new ThingTypeUID(BINDING_ID, "shellyrgbw2-white");
 
-    public static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS  = Collections
+    public static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS   = Collections
             .unmodifiableSet(Stream.of(THING_TYPE_SHELLY1, THING_TYPE_SHELLY2_RELAY, THING_TYPE_SHELLY2_ROLLER,
                     THING_TYPE_SHELLY4PRO, THING_TYPE_SHELLYPLUG, THING_TYPE_SHELLYPLUG, THING_TYPE_SHELLYPLUGS, THING_TYPE_SHELLYBULB_COLOR,
-                    THING_TYPE_SHELLYBULB_WHITE, THING_TYPE_SHELLYHT, THING_TYPE_SHELLYSENSE, THING_TYPE_SHELLYSMOKE, THING_TYPE_SHELLYRGW2)
+                    THING_TYPE_SHELLYBULB_WHITE, THING_TYPE_SHELLYHT, THING_TYPE_SHELLYSENSE, THING_TYPE_SHELLYSMOKE, THING_TYPE_SHELLYRGBW2_COLOR,
+                    THING_TYPE_SHELLYRGBW2_WHITE)
                     .collect(Collectors.toSet()));
 
-    public static final int               UPDATE_STATUS_INTERVAL      = 3; // check for updates every x sec
-    public static final int               UPDATE_SKIP_COUNT           = 5; // update every x triggers or when a key was pressed
+    public static final int               UPDATE_STATUS_INTERVAL       = 3; // check for updates every x sec
+    public static final int               UPDATE_SKIP_COUNT            = 5; // update every x triggers or when a key was pressed
 
     // List of all Configuration Properties
-    public static final String            CONFIG_DEVICEIP             = "deviceIp";
-    public static final String            CONFIG_IDENTITY             = "identity";
+    public static final String            CONFIG_DEVICEIP              = "deviceIp";
+    public static final String            CONFIG_IDENTITY              = "identity";
 
     // Relay
-    public static final String            CHANNEL_GROUP_RELAY_CONTROL = "relay";
-    public static final String            CHANNEL_RELAY_OUTPUT        = "output";
-    public static final String            CHANNEL_RELAY_TIMER         = "timer";
-    public static final String            CHANNEL_RELAY_OVERPOWER     = "overpower";
-    public static final String            CHANNEL_RELAY_TRIGGER       = "trigger";
+    public static final String            CHANNEL_GROUP_RELAY_CONTROL  = "relay";
+    public static final String            CHANNEL_RELAY_OUTPUT         = "output";
+    public static final String            CHANNEL_RELAY_TIMER          = "timer";
+    public static final String            CHANNEL_RELAY_OVERPOWER      = "overpower";
+    public static final String            CHANNEL_RELAY_TRIGGER        = "trigger";
 
     // Roller
-    public static final String            CHANNEL_GROUP_ROL_CONTROL   = "roller";
-    public static final String            CHANNEL_ROL_CONTROL_TURN    = "turn";
-    public static final String            CHANNEL_ROL_CONTROL_POS     = "position";
-    public static final String            CHANNEL_ROL_CONTROL_TIMER   = "timer";
-    public static final String            CHANNEL_ROL_CONTROL_STOPR   = "stopReason";
-    public static final String            CHANNEL_ROL_CONTROL_DIR     = "direction";
-    public static final String            CHANNEL_ROL_CONTROL_OVERT   = "overtemp";
-    public static final String            CHANNEL_ROL_CONTROL_CALIB   = "calibrating";
+    public static final String            CHANNEL_GROUP_ROL_CONTROL    = "roller";
+    public static final String            CHANNEL_ROL_CONTROL_TURN     = "turn";
+    public static final String            CHANNEL_ROL_CONTROL_POS      = "position";
+    public static final String            CHANNEL_ROL_CONTROL_TIMER    = "timer";
+    public static final String            CHANNEL_ROL_CONTROL_STOPR    = "stopReason";
+    public static final String            CHANNEL_ROL_CONTROL_DIR      = "direction";
+    public static final String            CHANNEL_ROL_CONTROL_OVERT    = "overtemp";
+    public static final String            CHANNEL_ROL_CONTROL_CALIB    = "calibrating";
 
     // Power meter
-    public static final String            CHANNEL_GROUP_METER         = "meter";
-    public static final String            CHANNEL_METER_CURRENTWATTS  = "currentWatts";
-    public static final String            CHANNEL_METER_LASTMIN1      = "lastPower1";
-    public static final String            CHANNEL_METER_LASTMIN2      = "lastPower2";
-    public static final String            CHANNEL_METER_LASTMIN3      = "lastPower3";
-    public static final String            CHANNEL_METER_TOTALWATTS    = "totalWatts";
-    public static final String            CHANNEL_METER_MAXPOWER      = "maxPower";
+    public static final String            CHANNEL_GROUP_METER          = "meter";
+    public static final String            CHANNEL_METER_CURRENTWATTS   = "currentWatts";
+    public static final String            CHANNEL_METER_LASTMIN1       = "lastPower1";
+    public static final String            CHANNEL_METER_LASTMIN2       = "lastPower2";
+    public static final String            CHANNEL_METER_LASTMIN3       = "lastPower3";
+    public static final String            CHANNEL_METER_TOTALWATTS     = "totalWatts";
+    public static final String            CHANNEL_METER_MAXPOWER       = "maxPower";
 
-    public static final String            CHANNEL_GROUP_LED_CONTROL   = "ledControl";
-    public static final String            CHANNEL_LED_STATUS_DISABLE  = "ledStatusDisable";
-    public static final String            CHANNEL_LED_POWER_DISABLE   = "ledPowerDisable";
+    public static final String            CHANNEL_GROUP_LED_CONTROL    = "ledControl";
+    public static final String            CHANNEL_LED_STATUS_DISABLE   = "ledStatusDisable";
+    public static final String            CHANNEL_LED_POWER_DISABLE    = "ledPowerDisable";
 
-    public static final String            CHANNEL_GROUP_SENSOR        = "sensor";
-    public static final String            CHANNEL_SENSOR_TEMP         = "temperature";
-    public static final String            CHANNEL_SENSOR_TUNIT        = "tempUnit";
-    public static final String            CHANNEL_SENSOR_HUM          = "humidity";
+    public static final String            CHANNEL_GROUP_SENSOR         = "sensor";
+    public static final String            CHANNEL_SENSOR_TEMP          = "temperature";
+    public static final String            CHANNEL_SENSOR_TUNIT         = "tempUnit";
+    public static final String            CHANNEL_SENSOR_HUM           = "humidity";
 
-    public static final String            CHANNEL_GROUP_BATTERY       = "batteryStatus";
-    public static final String            CHANNEL_SENSOR_BAT_LEVEL    = "batValue";
-    public static final String            CHANNEL_SENSOR_BAT_VOLT     = "batVoltage";
+    public static final String            CHANNEL_GROUP_BATTERY        = "batteryStatus";
+    public static final String            CHANNEL_SENSOR_BAT_LEVEL     = "batValue";
+    public static final String            CHANNEL_SENSOR_BAT_VOLT      = "batVoltage";
 
-    public static final String            CHANNEL_GROUP_BULB_CONTROL  = "bulb";
-    public static final String            CHANNEL_BULB_POWER          = "power";
-    public static final String            CHANNEL_BULB_DEFSTATE       = "defaultState";
-    public static final String            CHANNEL_BULB_AUTOON         = "autoOn";
-    public static final String            CHANNEL_BULB_AUTOOFF        = "autoOff";
-    public static final String            CHANNEL_BULB_TIMER          = "flipbackTimer";
+    public static final String            CHANNEL_GROUP_BULB_CONTROL   = "bulb";
+    public static final String            CHANNEL_BULB_POWER           = "power";
+    public static final String            CHANNEL_BULB_DEFSTATE        = "defaultState";
+    public static final String            CHANNEL_BULB_AUTOON          = "autoOn";
+    public static final String            CHANNEL_BULB_AUTOOFF         = "autoOff";
+    public static final String            CHANNEL_BULB_TIMER           = "flipbackTimer";
 
-    public static final String            CHANNEL_GROUP_COLOR_CONTROL = "color";
-    public static final String            CHANNEL_COLOR_RED           = "red";
-    public static final String            CHANNEL_COLOR_GREEN         = "green";
-    public static final String            CHANNEL_COLOR_BLUE          = "blue";
-    public static final String            CHANNEL_COLOR_WHITE         = "white";
-    public static final String            CHANNEL_COLOR_GAIN          = "gain";
-    public static final String            CHANNEL_COLOR_BRIGHTNESS    = "brightness";
-    public static final String            CHANNEL_COLOR_TEMP          = "temperature";
-    public static final String            CHANNEL_COLOR_EFFECT        = "effect";
+    public static final String            CHANNEL_GROUP_COLOR_CONTROL  = "color";
+    public static final String            CHANNEL_COLOR_RED            = "red";
+    public static final String            CHANNEL_COLOR_GREEN          = "green";
+    public static final String            CHANNEL_COLOR_BLUE           = "blue";
+    public static final String            CHANNEL_COLOR_WHITE          = "white";
+    public static final String            CHANNEL_COLOR_GAIN           = "gain";
+    public static final String            CHANNEL_COLOR_BRIGHTNESS     = "brightness";
+    public static final String            CHANNEL_COLOR_TEMP           = "temperature";
+    public static final String            CHANNEL_COLOR_EFFECT         = "effect";
 }
