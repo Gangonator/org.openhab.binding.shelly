f939e9306755ef185950c1c32f7bbe4ad5541c68
markus7017 <markus7017@gmail.com>
2019-08-28 22:29:31 +0200

    dummy-coap added

---
+++ src/main/java/org/openhab/binding/shelly/internal/coap/ShellyCoapListener.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.shelly.internal.coap;
+
+import org.openhab.binding.shelly.internal.config.ShellyThingConfiguration;
+import org.openhab.binding.shelly.internal.handler.ShellyHandler;
+
+/**
+ * The {@link ShellyCoapListener} handles the coap registration and events.
+ *
+ * @author Markus Michels - Initial contribution
+ * @author Hans-Jörg Merk - Initial contribution
+ */
+public class ShellyCoapListener {
+//    private final Logger                   logger = LoggerFactory.getLogger(ShellyCoapListener.class);
+//    private final ShellyThingConfiguration config;
+
+    public ShellyCoapListener(ShellyHandler thingHandler, ShellyThingConfiguration config) {
+//        this.config = config;
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+}
